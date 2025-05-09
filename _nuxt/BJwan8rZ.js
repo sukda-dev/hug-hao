6cc2c9336dac37682b8404cbea41581c03838d5a
Bank Outsourcify <bank@outsourcify.net>
2025-05-09 14:26:28 +0700

    Updates

--- _nuxt/BJwan8rZ.js
+++
@@ -1,1 +0,0 @@
-import{I as s}from"./CVVtmJfw.js";const i=s("/images/logo.png"),p=s("/images/line-qr.png");export{i as _,p as a};
---
+++ _nuxt/UurIn0H2.js
@@ -0,0 +1,1 @@
+import"./C8s69F6M.js";import{t,v as r,P as a}from"./QzcucaTJ.js";const c={__name:"default",setup(o){return(e,l)=>(r(),t("div",null,[a(e.$slots,"default")]))}};export{c as default};
